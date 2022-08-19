7460d3462e08c622483878f74e88f13694745933
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-08-19 15:49:53 -0300

    exericicio 2

---
+++ Fundamentos/Bloco4_introucao_a_JS_e_Logica/dia_2/exercicio2.js
@@ -0,0 +1,16 @@
+let num = 7;
+
+for ( let index = 1; index < 10; index += 1){
+    console.log(num*index);
+}
+
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+for(let index = 0; index < listaDeNomes.length; index +=1){
+    let mensensagem = 'Boas vindas, ' + listaDeNomes[index] + '!';
+    console.log(mensensagem);
+}
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
