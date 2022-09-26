40199d7fdbddfa0ab157b2813b59916bbffc2e40
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-26 19:01:20 -0300

    exercicio 2 feito

--- Fundamentos/bloco8_HoFs_JscriptES6/dia_3/sctip.js
+++ Fundamentos/bloco8_HoFs_JscriptES6/dia_3/sctip.js
@@ -76,4 +76,41 @@ function formatedBookNames(arr) {
   return result;
 }
 
-console.log(formatedBookNames(books))
+const expectedResult1 = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge(arr) {
+  const names = arr.map((element) => (
+    {age: element.releaseYear - element.author.birthYear ,
+     authour: element.author.name
+    }
+  ));
+    
+  return names.sort((a, b) => a.age - b.age);
+}
+
+console.log(nameAndAge(books));
