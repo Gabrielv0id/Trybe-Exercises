49e847b4914475fca01cf12fa6ce99244273a8d3
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-09 13:01:06 -0300

    dia 2

---
+++ front-end/js_e_testes/dia_2/fetch.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  // const result = await fetch(url)
+  //   .then((response) => response.json())
+	// 	.then((data) => data.value)
+	// 	.catch((error) => console.log(`Algo deu errado :( \n${error}`));
+	
+	try {
+  	const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke();
