977995886fafb576a07e91ef22c4f6eb6fc82dec
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-19 18:11:25 -0300

    exemplos da aula

--- front-end/intro_react/dia_1/testando-meu-computador/src/App.js
+++ front-end/intro_react/dia_1/testando-meu-computador/src/App.js
@@ -1,8 +1,16 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (<span className='Helloworld'>Hello, world!</span>);
+// function App() {
+//   return (<span className='Helloworld'>Hello, world!</span>);
+// }
+
+
+//criacao de um componente 
+class App extends Component {
+  render () {
+    return (<span className='Helloworld'>Hello, world!</span>);
+  }
 }
 
 export default App;
